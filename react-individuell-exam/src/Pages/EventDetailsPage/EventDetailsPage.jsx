b85fd2851b4fc3efe96e59829e2fd7f613fee380
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-05-02 09:37:06 +0200

    stylar detail page och knappen

--- react-individuell-exam/src/Pages/EventDetailsPage/EventDetailsPage.jsx
+++ react-individuell-exam/src/Pages/EventDetailsPage/EventDetailsPage.jsx
@@ -1,11 +1,21 @@
 import EventDetailed from "../../Components/EventDetailed/EventDetailed";
+import CartButton from "../../Components/CartButton/CartButton";
+import { useFetch } from "../../Hooks/useFetch";
+import "./eventDetailsPage.css";
 
 function EventDetailsPage() {
+  const { data: eventsData, isLoading, isError } = useFetch("https://santosnr6.github.io/Data/events.json");
+  const events = eventsData?.events || [];
+
+  if (isLoading) return <p>Laddar event...</p>;
+  if (isError) return <p>Ett fel uppstod vid hämtning av event.</p>;
+
   return (
-    <div>
-      <h1>Event Details Page</h1>
-      <p>This is the event details page.</p>
-      <EventDetailed />
+    <div className="event-page page-container">
+      <h1 className="page_title">Event</h1>
+      <p className="page_sub">You are about to score some tickets to</p>
+      <EventDetailed events={events}/>
+      <CartButton />
     </div>
   );
 }
--- react-individuell-exam/src/Pages/EventsPage/EventsPage.jsx
+++ react-individuell-exam/src/Pages/EventsPage/EventsPage.jsx
@@ -1,26 +1,13 @@
-import { useFetch } from "../../Hooks/useFetch";
 import EventsList from "../../Components/EventsList/EventsList.jsx";
 import "./eventsPage.css";
 
 function EventsPage() {
-  const { data: eventsData, isLoading, isError } = useFetch("https://santosnr6.github.io/Data/events.json");
-  const events = eventsData?.events || [];
-
-  console.log("Fetched events:", events);
-  
-  if (isLoading) return <p>Laddar event...</p>;
-  if (isError) return <p>Ett fel uppstod vid hämtning av event.</p>;
-
 
   return (
     <div className="events-page page-container">
-      <h1 className="events-page_title">Events</h1>
+      <h1 className="page_title">Events</h1>
       <p>Search</p>
-      <ul className="events-list">
-        {events.map((event) => (
-          <EventsList key={event.id} event={event} />
-        ))}
-      </ul>
+      <EventsList />
     </div>
   );
 }
