6e1186e9efef511b5980f76a76a9b03dff8415fb
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-05-03 22:47:42 +0200

    button done, start with localstorage

--- react-individuell-exam/src/Components/OrderCounter/OrderCounter.jsx
+++ react-individuell-exam/src/Components/OrderCounter/OrderCounter.jsx
@@ -1,8 +1,27 @@
+import useCounter from "../../stores/useCounter.js";
 import "./orderCounter.css";
 import 'line-awesome/dist/line-awesome/css/line-awesome.min.css';
 
 function OrderCounter({ event }) {
+  const { count, increase, decrease } = useCounter();
 
+  // localStorage.setItem("EventId", event.id);
+
+  // const eventsinfo = {
+  //   id: event.id,
+  //   name: event.name,
+  //   place: event.place,
+  //   when: {
+  //     date: event.when.date,
+  //     from: event.when.from,
+  //     to: event.when.to,
+  //   },
+  //   where: event.where,
+  //   price: event.price,
+  //   qauilty: count,
+  // };
+
+  // localStorage.setItem("Chosen", eventsinfo);
   
   if (!event) {
     return console.log("Event data is not available.");
@@ -11,14 +30,14 @@ function OrderCounter({ event }) {
     <section className="order-counter">
       <section className="order-counter_container">
         <section className="order-counter_header">
-          <h2 className="order-counter_total">{event.price} sek</h2>
+          <h2 className="order-counter_total">{event.price * count} sek</h2>
         </section>
         <section className="order-counter_buttons">
-          <button className="order-decrease_button">
+          <button onClick={decrease} className="order-decrease_button">
             <i className="las la-minus"></i>  
           </button>        
-          <p></p>
-          <button className="order-increase_button">
+          <p>{count}</p>
+          <button onClick={increase} className="order-increase_button">
             <i className="las la-plus"></i>
           </button>
         </section>
--- react-individuell-exam/src/stores/useEventData.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useEventData = create((set) => ({
-  event: [], // Initial state för event-data
-  setEventData: (newEventData) => set({ event: newEventData }), // Funktion för att uppdatera event-data
-}));
-
-export default useEventData;
